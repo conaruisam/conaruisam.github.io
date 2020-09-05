f364b4e00c547fe449da0c1bdc0b412150bc99be
Isam <conaruisam@gmail.com>
2020-09-05 20:40:53 +0200

    Añadir más texto a la parte de conocimientos

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function getEdad(dateString) {
+    let hoy = new Date()
+    let fechaNacimiento = new Date(dateString)
+    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear()
+    let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth()
+    if (
+      diferenciaMeses < 0 ||
+      (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
+    ) {
+      edad--
+    }
+    return edad
+  }
+
+ 
+  $(function(){
+	$('.contenedor-imagen').on('click', function(){
+		$('#modal').modal;
+		var ruta_imagen = ($(this).find('img').attr('src'));
+		$('#imagen-modal').attr('src', ruta_imagen);
+	});
+
+	$('#modal').on('click', function(){
+		$('#modal').modal('hide');
+	});
+})
